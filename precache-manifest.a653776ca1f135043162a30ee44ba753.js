7b8e33d5894f0246d9298554e94f5c6d0eaf19d9
suresh <itssuresh.js@gmail.com>
2019-07-19 13:25:36 +0530

    Updates

---
+++ precache-manifest.a653776ca1f135043162a30ee44ba753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd12e98e6336f259715f9d04f97fe27",
+    "url": "/monster-rolodex.git/index.html"
+  },
+  {
+    "revision": "c0b04615d0edc75932de",
+    "url": "/monster-rolodex.git/static/css/main.81c59b66.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c0b04615d0edc75932de",
+    "url": "/monster-rolodex.git/static/js/main.1f2574c3.chunk.js"
+  },
+  {
+    "revision": "f352b8512bb5c16a65b5",
+    "url": "/monster-rolodex.git/static/js/runtime~main.8c7a891a.js"
+  }
+]);
